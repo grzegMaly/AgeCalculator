e762e4e14e07f24dc9e1d46ac43ee57ea72a0f9b
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-08-15 18:57:54 +0200

    JS Logics

---
+++ Vanilla/script.js
@@ -0,0 +1,22 @@
+import {calcAge} from "./calcAge.js";
+
+const form = document.querySelector('form')
+const birthdayEl = document.getElementById('birthday');
+const resultEl = document.getElementById('result');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const birthdayValue = birthdayEl.value;
+    if (birthdayValue === "") {
+        alert("Please enter your birthday");
+        return;
+    }
+
+    const age = calcAge(birthdayValue);
+    if (!Number.isInteger(age) || age < 0) {
+        resultEl.textContent = 'Insert Correct Date';
+        return;
+    }
+    resultEl.textContent = `You are ${age} year${age > 1 ? "s" : ""} old`;
+});
