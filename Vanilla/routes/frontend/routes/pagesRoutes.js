d39ada15da55748d869ae882579315839dc9d915
grzegMaly <gmalysa@student.wsb-nlu.edu.pl>
2025-08-29 16:51:55 +0200

    Rename

--- Vanilla/routes/frontend/routes/pagesRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const viewController = require('../../../controllers/viewController');
-const authController = require('../../../controllers/authController');
-
-router.get('/', authController.protect, viewController.getCalculatorPage);
-
-/** @type {import('express').Router}*/
-module.exports = router;
